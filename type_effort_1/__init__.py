4aac8baa12fff371e2da637fd1103d7c25dad513
P Carmel Marie F. Zagre <108446176+appgpk@users.noreply.github.com>
2026-02-01 22:58:10 -0800

    Update __init__.py

--- type_effort_1/__init__.py
+++ type_effort_1/__init__.py
@@ -155,10 +155,11 @@ class Round1(Page):
         
         player.ball = draw_ball()
         player.multiplier = get_multiplier(player.chosen_type, player.ball)
-        if player.multiplier == 1 :
+        player.multiplier_display = str(player.multiplier)
+        """if player.multiplier == 1 :
             player.multiplier_display = "1"
         else : 
-            player.multiplier_display = "1/3"
+            player.multiplier_display = "1/3""""
 
         player.points = int(round(player.effort * player.multiplier))
         player.payoff = player.points * cu(2.50/300)
